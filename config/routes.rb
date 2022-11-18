e555c5af85b4c868ff3dde719f9861e9d3de79de
andgarzonmal <andgarzonmal@unal.edu.co>
2022-11-18 16:18:05 -0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "user#index"
+  root to: "users#index"
 end
