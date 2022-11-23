9e2ee1178ff7f54b27ec1547da05987b7330e48d
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-11-23 16:39:18 -0500

    Finished Endpoints for user signup and logout with JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   include Devise::JWT::RevocationStrategies::JTIMatcher
-         
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
