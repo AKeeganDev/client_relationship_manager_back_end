f25e7a6c45c3a3d484e2958210854609c87e3e4e
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-11-25 16:20:46 -0500

    fix linters

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,5 +3,6 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates_format_of :phone_number, with: /^(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}$/, multiline: true, allow_blank: true
+  validates_format_of :phone_number, with: /^(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}$/,
+                                     multiline: true, allow_blank: true
 end
