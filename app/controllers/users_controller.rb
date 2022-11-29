bab5764269e29b55723bf854b0dece5448631258
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-11-29 16:12:33 -0500

    first attempt: adding CRUD to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
   def index
-    render json: { user: User.all }, status: :ok
+    render json: { user: current_user.first_five_contacts, contacts: current_user.first_five_contacts }, status: :ok
   end
 
   def show
-    render json: { user: current_user }, status: :ok
+    render json: { user: current_user.contacts }, status: :ok
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email)
   end
 end
