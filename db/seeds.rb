d6f8d2f115f4149b8aa92c3280c6b6603e2d361e
andgarzonmal <andgarzonmal@unal.edu.co>
2022-11-30 10:51:06 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# User.create(name:"Andres", username:"Andgarzonmal", email:"andgarzonmal@unal.edu.co", password:"123456")
+User.create(name:"Andres", username:"Andgarzonmal", email:"andgarzonmal@unal.edu.co", password:"123456")
 user = User.first
 Contact.create(name: "Aaron 2", phone_number: "+1 123 456 7899",email:"aaron@aaron.com", user_id: user.id)
 Contact.create(name: "Aaron 3", phone_number: "+1 123 456 7899",email:"aaron@aaron.com", user_id: user.id)
