cdf387187e3dd9094e956f901f705a125347099c
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-12-02 11:34:33 -0500

    fix contact controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     # @user = User.find(params[:id])
     # render json: { user: @user, contacts: @user.contacts }, status: :ok
-    render json: { user: current_user.first_five_contacts }, status: :ok
+    render json: { user: current_user, contacts: current_user.contacts }, status: :ok
   end
 
   def update
