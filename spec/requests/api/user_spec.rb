838d71b12705212014bb72c19dfc5b6b96de451b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-12-12 16:26:30 -0500

    Add users get specs

--- spec/requests/api/user_spec.rb
+++ spec/requests/api/user_spec.rb
@@ -1,34 +1,37 @@
 require 'swagger_helper'
 
 RSpec.describe 'User', type: :request do
-  path user_path do
-    post(
+  path '/users/{id}' do
+    get(
       'Provides data on the User\'s basic account information and a list of all contacts logged in the database'
     ) do
       tags 'User'
       consumes 'application/json'
       parameter name: 'Authorization', in: :header, required: true
-      response(200, 'Logged out sucessfully') do
-        run_test!
-      end
-
-      response(401, 'Couldn\'t find an active session.') do
+      response(200, '') do
         run_test!
       end
     end
   end
 
-  path '/logout' do
-    delete('Logs a user out of their current session') do
+  path '/users/{id}' do
+    patch('Allows user to update its basic information. Only include the fields you want to update.') do
       tags 'User'
       consumes 'application/json'
       parameter name: 'Authorization', in: :header, required: true
-
-      response(200, 'Logged out sucessfully') do
-        run_test!
-      end
-
-      response(401, 'Couldn\'t find an active session.') do
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            properties: {
+              email: { type: :string },
+              name: { format: :string },
+              username: { format: :string }
+            }
+          }
+        },
+      }
+      response(200, '') do
         run_test!
       end
     end
